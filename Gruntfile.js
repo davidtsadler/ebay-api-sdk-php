98553d4f58ffc4a2291c0b3a2d2b3159aebdc8e9
David T. Sadler <davidtsadler@googlemail.com>
2013-09-11 12:24:58 +0100

    Add jshint.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,32 @@
+'use strict';
+
 module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
+        jshint: {
+            all: ['Gruntfile.js', 'test/*.js'],
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true,
+                node: true
+            }
+        },
+
         nodeunit: {
             tests: ['test/*_test.js']
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-    grunt.registerTask('test', ['nodeunit']);
+    grunt.registerTask('test', ['jshint', 'nodeunit']);
 };
